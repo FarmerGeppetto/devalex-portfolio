8894394c00bea055405ca349bf6e24cb6bd000e7
Devalex <devalex.programming@gmail.com>
2024-11-24 15:13:54 +0100

    error fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const config = {
     reactStrictMode: true,
     eslint: {
         ignoreDuringBuilds: true,
     },
 };
 
-export default nextConfig;
+export default config;
